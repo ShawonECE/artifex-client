97651760b5fed4a9112534f35ffaf8bb609f2408
ShawonECE <send2shawon@gmail.com>
2024-04-28 10:22:18 +0600

    My craft section completed

---
+++ src/components/MyCraftCard.jsx
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+import { CiStar } from 'react-icons/ci';
+
+const MyCraftCard = ({ craft }) => {
+    const { image, item_name, price, rating, customization, stock_status, short_description } = craft;
+    return (
+        <div className="card bg-base-100 shadow-xl">
+            <figure><img src={image} alt="Craft" /></figure>
+            <div className="card-body">
+                <h2 className="text-2xl font-bold">{item_name}</h2>
+                <div className='flex items-center'>
+                    <p className='text-lg font-semibold'>${price}</p>
+                    <div className={`badge text-white ${stock_status === 'In stock' ? 'bg-orange-400' : 'bg-green-400'}`}>{stock_status}</div>
+                </div>
+                <p>{short_description}</p>
+                <p className='text-lg font-medium'>Customizable: {customization}</p>
+                <div className="flex items-center gap-1">
+                    <CiStar className='text-xl' />
+                    <p className='text-lg font-medium'>{rating}</p>
+                </div>
+                <div className="card-actions justify-end">
+                    <button className="btn bg-slate-800 text-white">Update</button>
+                    <button className="btn bg-slate-800 text-white">Delete</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+MyCraftCard.propTypes = {
+    craft: PropTypes.object.isRequired
+};
+
+export default MyCraftCard;
--- src/components/MyCrafts.jsx
+++ src/components/MyCrafts.jsx
@@ -1,12 +1,33 @@
 import { Helmet } from "react-helmet-async";
+import { useContext, useState } from 'react';
+import { AuthContext } from "./AuthProvider";
+import { useEffect } from 'react';
+import MyCraftCard from "./MyCraftCard";
 
 const MyCrafts = () => {
+    const {user} = useContext(AuthContext);
+    const [items, setItems] = useState([]);
+    useEffect(() => {
+        fetch(`http://localhost:3000/sculptures/user/${user.email}`)
+        .then(res => res.json())
+        .then(data => setItems(data))
+    }, [user]);
     return (
         <div>
             <Helmet>
                 <title>Artifex | My Crafts</title>
             </Helmet>
-            My Crafts
+            {
+                !items.length && <h1 className="text-3xl font-bold text-center mt-10">You did not add any crafts yet</h1>
+            }
+            {
+                items.length && <h1 className="text-3xl font-bold text-center mt-10">Crafts added by you</h1>
+            }
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-8">
+                {
+                    items.map(craft => <MyCraftCard key={craft._id} craft={craft}></MyCraftCard>)
+                }
+            </div>
         </div>
     );
 };
