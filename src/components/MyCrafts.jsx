af5363266cb864429ed5899200ac5fe4e1798cef
ShawonECE <send2shawon@gmail.com>
2024-04-28 23:05:30 +0600

    filter functionality implemented in my crafts section

--- src/components/MyCrafts.jsx
+++ src/components/MyCrafts.jsx
@@ -8,13 +8,32 @@ import swal from 'sweetalert';
 
 const MyCrafts = () => {
     const { user } = useContext(AuthContext);
+    const [selectedValue, setSelectedValue] = useState('Filter');
     const [items, setItems] = useState([]);
+    const [filtered, setFiltered] = useState([]);
     const navigate = useNavigate();
     useEffect(() => {
         fetch(`http://localhost:3000/sculptures/user/${user.email}`)
             .then(res => res.json())
-            .then(data => setItems(data))
+            .then(data => {
+                setItems(data);
+                setFiltered(data);
+            })
     }, [user]);
+    const handleFilter = (e) => {
+        const value = e.target.value;
+        setSelectedValue(value);
+
+        if (value === "No filter") {
+            setFiltered(items);
+        } else if (value === "Customizable") {
+            const current = items.filter(item => item.customization === "yes");
+            setFiltered(current);
+        } else {
+            const current = items.filter(item => item.customization === "no");
+            setFiltered(current);
+        }
+    };
     const handleDelete = (id) => {
         swal({
             title: "Are you sure?",
@@ -59,9 +78,17 @@ const MyCrafts = () => {
             {
                 items.length && <h1 className="text-3xl font-bold text-center mt-10">Crafts added by you</h1>
             }
+            <div className="flex justify-center mt-8">
+                <select value={selectedValue} onChange={handleFilter} className="select max-w-xs bg-gray-100 text-lg font-semibold">
+                    <option disabled>Filter</option>
+                    <option>No filter</option>
+                    <option>Customizable</option>
+                    <option>Non-Customizable</option>
+                </select>
+            </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-8">
                 {
-                    items.map(craft => <MyCraftCard key={craft._id} craft={craft} handleDelete={handleDelete} handleUpdate={handleUpdate}></MyCraftCard>)
+                    filtered.map(craft => <MyCraftCard key={craft._id} craft={craft} handleDelete={handleDelete} handleUpdate={handleUpdate}></MyCraftCard>)
                 }
             </div>
         </div>
