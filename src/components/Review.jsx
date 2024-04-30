aa742bb9d734c43456e919ec31a9387b29c728d7
ShawonECE <send2shawon@gmail.com>
2024-04-30 13:53:18 +0600

    fetching data for review section added

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,11 +1,29 @@
-import reviews from '../assets/review';
+import { useEffect, useState } from 'react';
 import ReviewCard from './ReviewCard';
 
 const Review = () => {
+    const [reviews, setReviews] = useState([]);
+    const [loaded, setLoaded] = useState(false);
+    useEffect(() => {
+        fetch('https://artifex-server.vercel.app/reviews')
+        .then(response => response.json())
+        .then(data => {
+            setReviews(data);
+            setLoaded(true);
+        });
+    }, []);
     return (
         <div className="mt-16">
             <h1 className="text-center dark:text-white text-4xl font-bold">Our Inspirations</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-8">
+                {
+                    !loaded &&
+                    <>
+                        <div className="skeleton h-32"></div>
+                        <div className="skeleton h-32"></div>
+                        <div className="skeleton h-32"></div>
+                    </>
+                }
                 {
                     reviews.map((review, idx) => <ReviewCard key={idx} review={review}></ReviewCard>)
                 }
